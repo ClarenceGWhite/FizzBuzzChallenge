52f19372c7a401366b8fa15caa7d1b410992fd61
clarence <cgwhite76@gmail.com>
2022-11-14 14:07:58 -0600

    Final Version

---
+++ FizzBuzzChallenge/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace FizzBuzzChallenge
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter a whole number to see if it is divisible by 3 or 5...");
+            int number = int.Parse(Console.ReadLine());
+            FizzBuzz(number);
+
+        }   
+
+            public static string FizzBuzz(int number)
+            {
+                var answer = "";
+
+                if (number % 3 == 0)
+                {
+                    answer = "fizz";
+                    Console.WriteLine("This number is divisible by 3.");
+                }
+
+                if (number % 5 == 0)
+                {
+                    answer += "buzz";
+                    Console.WriteLine("This number is divisible by 5.");
+                }
+
+                Console.WriteLine(answer);
+                return answer;
+
+
+            }
+
+
+            
+
+        
+    }
+}
